ea5f0514c0ec6824c5d3506cf10b79ab90fd142e
Marcin Przewięźlikowski <m.przewie@gmail.com>
2024-06-12 11:40:04 +0200

    cleanup

--- scripts/resnet18_5_clients.sh
+++ scripts/resnet18_5_clients.sh
@@ -31,30 +31,4 @@ arch=ResNet18
          done
  done
 
-#non_iid_list="0.2"
-#cutlayer_list="8"
-#num_client_list="20"
-#dataset=cifar10
-#loss_threshold=0.0
-#ressfl_alpha=0.0
-#bottleneck_option=None
-#batch_size=20
-#client_sample_ratio=0.25
-#avg_freq=10
-#divaware_lambda=1.0
-#
-#for num_client in $num_client_list; do
-#        for noniid_ratio in $non_iid_list; do
-#                for cutlayer in $cutlayer_list; do
-#                        output_dir="./outputs/r-mocosfl${moco_version}_${arch}_${dataset}_cut${cutlayer}_bnl${bottleneck_option}_client${num_client}_nonIID${noniid_ratio}_af${avg_freq}_fam" #da${divaware_lambda}"
-#                        python run_sflmoco_repro.py --num_client ${num_client} --lr ${lr} --cutlayer ${cutlayer} --num_epoch ${num_epoch}\
-#                                --noniid_ratio ${noniid_ratio} --output_dir ${output_dir}\
-#                                --moco_version ${moco_version} --arch ${arch} --dataset ${dataset} --loss_threshold ${loss_threshold}\
-#                                --ressfl_alpha ${ressfl_alpha} --bottleneck_option ${bottleneck_option} --batch_size ${batch_size}\
-#                                --client_sample_ratio ${client_sample_ratio} --avg_freq ${avg_freq} --div_lambda ${divaware_lambda} \
-#                                --divergence_measure --seed 1239 --fedavg-momentum #--gmatching oracle-most-similar --gmatching-ngrads 19
-#
-#                done
-#        done
-#done
 ## for test, add --resume --attack
